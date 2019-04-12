64b8bf9da314e3f554a377fc961f8c5687eaa711
Mathieu Garon <mathieugaron91@gmail.com>
2019-04-12 08:49:35 -0400

    Fix quaternion bug with to_parameters

--- ulaval_6dof_object_tracking/utils/transform.py
+++ ulaval_6dof_object_tracking/utils/transform.py
@@ -98,16 +98,16 @@ class Transform:
     def to_parameters(self, isDegree=False, isQuaternion=False, rodrigues=True):
         x, y, z = self.matrix[0:3, 3]
         if rodrigues:
-            a, b, c = ea.rodrigues_inverse(self.matrix[0:3, 0:3])
+            rx, ry, rz = ea.rodrigues_inverse(self.matrix[0:3, 0:3])
         else:
-            a, b, c = ea.mat2euler(self.matrix[0:3, 0:3])
+            rx, ry, rz = ea.mat2euler(self.matrix[0:3, 0:3])
         if isDegree:
-            a = math.degrees(a)
-            b = math.degrees(b)
-            c = math.degrees(c)
-        ret = [x, y, z, a, b, c]
+            rx = math.degrees(rx)
+            ry = math.degrees(ry)
+            rz = math.degrees(rz)
+        ret = [x, y, z, rx, ry, rz]
         if isQuaternion:
-            qx, qy, qz, qw = ea.euler2quat(a, b, c)
+            qx, qy, qz, qw = ea.euler2quat(x=rx, y=ry, z=rz)
             ret = [x, y, z, qx, qy, qz, qw]
         return np.array(ret)
 
